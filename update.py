56beb70c6e2824073e6df15fc20100c7212c9ec0
TheMerkyShadow <themerkyshadows@gmail.com>
2019-11-13 23:46:02 -0500

    big

--- update.py
+++ update.py
@@ -8,27 +8,45 @@ import time
 from urllib.request import urlopen
 from pathlib import Path
 
-class Config():
+def notify(text,delay=2):
+    print(text)
+    time.sleep(delay)
+    return
+
+class JsonConfig():
     def __init__(self,dir="config.json"):
         self.Path = Path(dir)    
-        self.Table = self.Decode()   
+        self.Table = self.Decode()
     def Decode(self):
         return json.loads(self.Path.read_text())
     def Encode(self):
         return json.dumps(self.Table,indent=4)         
     def __getitem__(self, item):
-        return self.Table[item]
+        return self.Table[item] 
     def __setitem__(self, item, value):
         self.Table[item] = value
     def Close(self):
         self.Path.write_text( self.Encode() )
-        
-CONFIG = Config()
+    def Reload(self):
+        return self.Decode()        
 
-def notify(text,delay=2):
-    print(text)
-    time.sleep(delay)
-    return
+class Git():
+    def __init__(self,CONFIG):
+        self.Repo = CONFIG["repo"]     
+        self.Url = ( "https://api.github.com/repos" +self.Repo )
+        self.Old = CONFIG["commit"]
+        self.New = self.latest()
+    def fetch(self,api):
+        return json.load(urlopen(self.Url+api))
+    def latest(self):
+        return self.fetch("branches/master")["commit"]["sha"]
+    def diff(self):
+        if self.Old == "" or self.Old == self.New:
+            return None
+        return self.fetch( "compare/" +self.Old +"..." +self.New )["files"]
+    
+CONFIG = JsonConfig()
+GIT = Git( CONFIG )
 
 def Minecraft(minecraft=None):
     if minecraft is None:
@@ -38,32 +56,18 @@ def Minecraft(minecraft=None):
         CONFIG["minecraft"] = minecraft
     return minecraft
 
-def GitDiff():    
-    repo = CONFIG["repo"]
-    api = ( "https://api.github.com/repos" +repo ) 
-    fetch = (api +"branches/master")  
-    new = json.load(urlopen(fetch))["commit"]["sha"]
-    old = CONFIG["commit"]
-    if old == "":
-        old = new
-    elif( old == new ):
-        return ( old, None, new )
-    fetch = ( api +"compare/" +old +"..." +new )
-    return ( old, json.load(urlopen(fetch))["files"], new )
-    
-def GitSync(backup=False):
+def GitSync():
     notify( "Verification Starting")
-    old, diff, new = GitDiff()
+    diff = GIT.diff()
     if diff:
-        exclude = CONFIG["exclude"]
         for file in diff:
             name = file["filename"]
             path = Path(name)
             parent = path.parent         
             raw = file["raw_url"]
             status = file["status"]
-            if name in exclude:
-                continue
+            if name == exclude:
+                continue                
             parent.mkdir(parents=True, exist_ok=True)
             if status == "added" or status == "modified":              
                 temp = Path(wget.download(raw))
@@ -72,17 +76,16 @@ def GitSync(backup=False):
                 previous = Path(file["previous_filename"])
                 if previous.is_file():
                     previous.replace(path)
-            elif status == "removed":
-                if path.is_file():
-                    path.unlink()
-                if parent.is_dir():
-                    empty = list(os.scandir(parent)) == 0
-                    if empty:
-                        parent.rmdir()
-
+                elif status == "removed":
+                    if path.is_file():
+                        path.unlink()
+                    if parent.is_dir():
+                        empty = list(os.scandir(parent)) == 0
+                        if empty:
+                            parent.rmdir()
+                            
             print( status, name )
 
-
     notify( "Verification Complete!" )
-    CONFIG["commit"] = new    
+    CONFIG["commit"] = GIT.New
     CONFIG.Close()
