726a899515c4a9273ad039652a398d86a53bb031
TheMerkyShadow <themerkyshadows@gmail.com>
2019-11-15 21:06:38 -0500

    fix

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import sys
 import subprocess
 import traceback
 
-import update
-from playsound import playsound
-
 try:
-    update.GitSync()
-    playsound('startup.mp3')    
+    import update
+    update.GitSync()   
 except Exception as e:
     print(traceback.format_exc())
     input( "Please Press Enter To Continue: " )
-    pass
 finally:
     subprocess.Popen([update.Minecraft(),"--workDir","dawn"])
     sys.exit()
