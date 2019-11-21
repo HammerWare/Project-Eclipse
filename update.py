3184f7f6a960e08ae32a8cfe0ef277a232cfdeee
TheMerkyShadow <themerkyshadows@gmail.com>
2019-11-21 00:52:15 -0500

    Update update.py

--- update.py
+++ update.py
@@ -78,7 +78,7 @@ def GitSync():
             raw = file["raw_url"]
             status = file["status"]
             parent.mkdir(parents=True, exist_ok=True)
-            if exclude.__contains__(name) or name.startswith(".git"):
+            if name in ','.join(exclude) or name.startswith(".git"):
                 continue
             if status == "added" or status == "modified":
                 temp = Path(wget.download(raw))
