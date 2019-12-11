433f0684b5070082a554bda46994ccb687101ad6
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2019-12-11 16:34:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import update
 update.GitSync()
 
 def Play(self):
-    subprocess.Popen([update.Minecraft(), '--workDir', 'dawn'])
+    subprocess.Popen( [ update.Minecraft(), '--workDir', 'dawn' ], close_fds=True )
 
 def Discord():
     print("discord")
