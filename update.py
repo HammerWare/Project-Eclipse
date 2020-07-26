8d7c47432b89a3a6d06b8e22b78f65908f3ffe9d
TheMerkyShadow <themerkyshadows@gmail.com>
2020-07-26 03:05:42 -0400

    Thank heavens

--- update.py
+++ update.py
@@ -8,7 +8,6 @@ import time
 import traceback
 
 from tkinter import *
-from tkinter import filedialog
 from tkinter import messagebox
 
 import urllib
@@ -50,20 +49,14 @@ class GitManager:
         try:              
             return json.load(urlopen(url))
         except urllib.error.URLError as e:
-            Notify('\n' + str(e))
-    def Download(self, path, branch="/master/"):
-        url = ( 'https://raw.githubusercontent.com/' )
-        url += ( self.Repo +branch +path )
-        temp = Path(wget.download(url))
-        temp.replace(path)
-        return temp
+            Notify( traceback.format_exc() )
     def Latest(self):
         return self.APIGateway('/branches/master')['commit']['sha']
     def Diff(self):
         self.Old = self.Config['commit']        
         self.New = self.Latest()
         if not self.Old.isalnum():
-            self.Old = self.New
+            self.Old = self.New        
         if self.Old == self.New:
             return None
         return self.APIGateway('/compare/' + self.Old + '...' + self.New)['files']
@@ -73,7 +66,8 @@ class GitManager:
         if diff:
             for info in diff:
                 path = Path( info["filename"] )
-                url = info["raw_url"]  
+                url = info["raw_url"]
+                url = url.replace("%20", " ") #weird git space bug in url
                 if path.name == "dawn.exe":
                     continue
                 
@@ -81,24 +75,30 @@ class GitManager:
                 parent.mkdir(parents=True, exist_ok=True)
 
                 status = info["status"]
-                if status == "renamed":
-                    previous = Path( info["previous_filename"] )
-                    if previous.is_file():
-                        previous.unlink()
-                    status = "added"
-                if status == "added" or status == "modified":
-                    temp = Path(wget.download(url))
-                    temp.replace(path)   
-                elif status == "removed":
-                    if path.is_file():
-                        path.unlink()
-                    if parent.is_dir():
-                        empty = list(os.scandir(parent)) == 0
-                        if empty:
-                            parent.rmdir()
+                try:
+                    if status == "added" or status == "modified":
+                        temp = Path(wget.download(url))
+                        temp.replace(path)
+                    elif status == "renamed":
+                        previous = Path( info["previous_filename"] )
+                        if previous.is_file():
+                            previous.rename(path)                        
+                    elif status == "removed":
+                        if path.is_file():
+                            path.unlink()
+                        if parent.is_dir():
+                            empty = list(os.scandir(parent)) == 0
+                            if empty:
+                                parent.rmdir()
+                                
+                except Exception as e:
+                    print( "ERROR:", status, path )
+                    Notify( traceback.format_exc() )
+                    sys.exit()
+                    
+                print( status, path )
 
-                print( path, status )
-
+        self.Config.Reload()        
         self.Config["commit"] = self.New
         Notify("Verification Complete" ) 
         return
@@ -130,11 +130,4 @@ def Minecraft():
     return reg.Valid
         
 if __name__ == '__main__':
-    try:
-        import menu
-    except Exception as e:
-        Notify(traceback.format_exc())
-        git = GitManager()
-        git.Download( "update.py")
-        git.Download( "menu.py")
-    
+    import menu
